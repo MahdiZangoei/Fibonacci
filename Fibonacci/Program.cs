ea238262e03145975ec9e05cbc00511a22936f51
MahdiZangoei <mahdizangoei1@gmail.com>
2023-05-30 18:04:58 +0200

    upload

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Enter a Number :");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(Fib(n));
+            Console.WriteLine("\n \n \n     Press any key for exit...");
+
+            Console.ReadKey();
+        }
+        static int Fib(int n) 
+        {
+            if (n <= 1) 
+            {
+                return n;
+            }
+            else 
+            {
+                return Fib(n - 1) + Fib(n - 2);
+            }
+        }
+    }
+}
